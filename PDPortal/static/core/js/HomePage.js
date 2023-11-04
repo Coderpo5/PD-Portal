1c91c89bb33c84b67d6331cace42c8c75e20204c
Hrishikeshan A Namboodiri <118446149+Coderpo5@users.noreply.github.com>
2023-11-04 23:33:21 +0530

    Add files via upload

---
+++ PDPortal/static/core/js/HomePage.js
@@ -0,0 +1,7 @@
+function reg(){
+    document.getElementsByClassName("InstituteTeamCont")[0].style.display = "block";
+    // console.log("chal")
+};
+function cross(){
+    document.getElementsByClassName("InstituteTeamCont")[0].style.display = "none";
+};
