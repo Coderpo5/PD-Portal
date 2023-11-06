0acdc558916838f699c1a8a1a6b0fdc0affcaf85
Hrishikeshan A Namboodiri <118446149+Coderpo5@users.noreply.github.com>
2023-11-06 23:20:20 +0530

    Delete PDPortal directory

--- PDPortal/static/core/js/HomePage.js
+++
@@ -1,7 +0,0 @@
-function reg(){
-    document.getElementsByClassName("InstituteTeamCont")[0].style.display = "block";
-    // console.log("chal")
-};
-function cross(){
-    document.getElementsByClassName("InstituteTeamCont")[0].style.display = "none";
-};
