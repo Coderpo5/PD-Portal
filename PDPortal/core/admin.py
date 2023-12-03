f2cb5a1ca98afb206bcde1cc77b549cbf645194c
Hrishikeshan A Namboodiri <118446149+Coderpo5@users.noreply.github.com>
2023-12-03 15:48:05 +0530

    Delete PDPortal directory

--- PDPortal/core/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import institutionalTeam_Contingent, Cross_Open, Independent_Adjudicator
-
-admin.site.register(institutionalTeam_Contingent)
-admin.site.register(Cross_Open)
-admin.site.register(Independent_Adjudicator)
